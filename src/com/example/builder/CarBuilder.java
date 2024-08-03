668618050178e2b2a39427f5737af09d9ba02217
Kotan-java <girlhard@ukr.net>
2024-08-03 11:27:50 +0300

    Initial commit

---
+++ src/com/example/builder/CarBuilder.java
@@ -0,0 +1,9 @@
+package com.example.builder;
+// Интерфейс Builder. Это интерфейс, который определяет методы для установки различных частей автомобиля
+//(двигатель, трансмиссия, колеса) и метод для сборки конечного объекта (build).
+public interface CarBuilder {
+    CarBuilder setEngine(String engine);
+    CarBuilder setTransmission(String transmission);
+    CarBuilder setWheels(int wheels);
+    Car build();
+}
